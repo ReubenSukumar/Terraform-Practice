29adad7ed3e5fc622de8bd5a55a0c2a4af67848a
Reuben <reubenraynold@gmail.com>
2026-03-16 18:01:26 +0530

    Updated the main.tf

--- EC2 instance creation/main.tf
+++ EC2 instance creation/main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 resource "aws_instance" "Trial-Instance" {
   ami             = "ami-0b6c6ebed2801a5cb"     # Caution: Use 'free tier eligible'
   instance_type   = "t3.micro"                  # Caution: Use 'free tier eligible'
-  security_groups = ["sg-0e15788c55312345"]    # If Security Group is already created mention else remove this field 
+  security_groups = ["sg-0e15788c55312345"]     # If Security Group is already created mention else remove this field 
   subnet_id       = "subnet-04bc3d5190e678945"  # Mention the created subnet else remove this field
   tags = {
     Name = "Terraform-Test-Instance"            # Instance Name
   }
-  key_name                    = "Test"      # .pem file name
+  key_name                    = "Test"          # .pem file name
   associate_public_ip_address = true            # Auto assign public IP is enabled
 }
 
