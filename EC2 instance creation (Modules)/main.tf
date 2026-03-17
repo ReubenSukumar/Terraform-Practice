fb24b79fd76ad64ef58b708c55c48574fb6a42f5
Reuben <reubenraynold@gmail.com>
2026-03-17 17:35:31 +0530

    Updated main.tf

--- EC2 instance creation (Modules)/main.tf
+++ EC2 instance creation (Modules)/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"                                  # Region where EC2 will be deployed
 }
 
-module "Trial_Instance" {
-  source           = "./EC2_Module"
-  key_pair         = "Treasure"
-  EC2_name         = "Terraform-Instance-Module-1"
-  assign_public_ip = true
+module "Trial_Instance" {                               # Module name used in EC2_Module/main.tf
+  source           = "./EC2_Module"                     # Path of module
+  key_pair         = "Treasure"                         # Key pair Name
+  EC2_name         = "Terraform-Instance-Module-1"      # Instance Name
+  assign_public_ip = true                               # Assign Public IP or not
 }
